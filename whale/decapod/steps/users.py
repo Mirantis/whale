85af1169c4763a1eeaedded30137d7ee0b7631ba
Alina Nesterova <anesterova@mirantis.com>
2017-03-06 12:05:25 +0400

    Add test to create, update and delete user
    These tests provide creating of user, updating it with new name and
    then deleting it
    
    Change-Id: I82b242a734bc91e3b7e98753afa69265d2167a79

--- whale/decapod/steps/users.py
+++ whale/decapod/steps/users.py
@@ -17,8 +17,10 @@ User steps
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from hamcrest import assert_that, empty, equal_to, is_not  # noqa H301
+from hamcrest import (assert_that, empty, equal_to, has_entries,
+                      is_not)  # noqa H301
 from stepler.third_party import steps_checker
+from stepler.third_party import utils
 
 from whale import base
 
@@ -31,14 +33,14 @@ class UserSteps(base.BaseSteps):
     """User steps."""
 
     @steps_checker.step
-    def create_user(self, user_login, user_email, user_full_name,
-                    role_id=None, check=True, **kwargs):
+    def create_user(self, user_login=None, user_email=None,
+                    user_full_name=None, role_id=None, check=True, **kwargs):
         """Step to create new user.
 
         Args:
-            user_login (str): the new login of the user
-            user_email (str): the new email of the user
-            user_full_name (str): the new full name of the user
+            user_login (str|None): the new login of the user
+            user_email (str|None): the new email of the user
+            user_full_name (str|None): the new full name of the user
             role_id (str): the new role id of the user
             check (bool): flag whether to check step or not
             **kwargs: any suitable keyword arguments
@@ -49,6 +51,9 @@ class UserSteps(base.BaseSteps):
         Raises:
             TimeoutExpired|AssertionError: if check was triggered to an error
         """
+        user_login = user_login or next(utils.generate_ids('user'))
+        user_email = user_email or (user_login + '@example.com')
+        user_full_name = user_full_name or next(utils.generate_ids('name'))
         user = self._client.create_user(login=user_login,
                                         email=user_email,
                                         full_name=user_full_name,
@@ -63,6 +68,50 @@ class UserSteps(base.BaseSteps):
             if role_id:
                 assert_that(user["data"]["role_id"], equal_to(role_id))
 
+        return user
+
+    @steps_checker.step
+    def update_user(self, user, new_data, check=True, **kwargs):
+        """Step to update user.
+
+        Args:
+            user (dict|str): user or its id
+            new_data (dict): new data for user
+            check (bool): flag whether to check step or not
+            **kwargs: any suitable keyword arguments
+
+        Returns:
+            user (dict): user model
+
+        Raises:
+            AssertionError: if check was triggered to an error
+        """
+        if not isinstance(user, dict):
+            user = self.get_user(user)
+        user['data'].update(new_data)
+        user = self._client.update_user(user, **kwargs)
+        if check:
+            assert_that(user['data'], has_entries(new_data))
+        return user
+
+    @steps_checker.step
+    def get_user(self, user_id, check=True, **kwargs):
+        """Step to get user.
+
+        Args:
+            user_id (str): user id
+            check (bool): flag whether to check step or not
+            **kwargs: any suitable keyword arguments
+
+        Returns:
+            user(dict): user model
+
+        Raises:
+            AssertionError: if check was triggered to an error
+        """
+        user = self._client.get_user(user_id, **kwargs)
+        if check:
+            assert_that(user['id'], equal_to(user_id))
         return user
 
     @steps_checker.step
---
+++ whale/decapod/tests/test_users.py
@@ -0,0 +1,86 @@
+"""
+----------
+User tests
+----------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from stepler.third_party import utils
+
+
+@pytest.mark.idempotent_id('a9c69976-e661-42bb-9fbc-9afa3d30f3e8')
+def test_create_delete_user(user_steps):
+    """**Scenario:** Check user creation and deletion.
+
+    **Setup:**
+
+    #. Create user
+    #. Delete user
+    """
+    user = user_steps.create_user()
+    user_steps.delete_user(user['id'])
+
+
+@pytest.mark.idempotent_id('28eb8dbb-a5ae-480a-a09a-1694b08736bc')
+def test_update_user(user, user_steps):
+    """**Scenario:** Check updating of user.
+
+    **Setup:**
+
+    #. Create user
+
+    **Steps:**
+
+    #. Update user with new name
+
+    **Teardown:**
+
+    #. Delete user
+    """
+    new_data = {'login': next(utils.generate_ids('login')),
+                'email': next(utils.generate_ids('email')) + '@example.com',
+                'full_name': next(utils.generate_ids('new_name'))}
+    user_steps.update_user(user, new_data)
+
+
+@pytest.mark.idempotent_id('af7aa0fe-a9ae-48ce-9ad9-a8ae0744d75b')
+def test_get_user(user, user_steps):
+    """**Scenario:** Check getting of user.
+
+    **Setup:**
+
+    #. Create user
+
+    **Steps:**
+
+    #. Get user by id
+
+    **Teardown:**
+
+    #. Delete user
+    """
+    user_steps.get_user(user['id'])
+
+
+@pytest.mark.idempotent_id('a5d6e791-6f9e-4405-ae50-e38c32a80968')
+def test_list_user(user_steps):
+    """Scenario:** Check getting of all users.
+
+    **Steps:**
+
+    #. List all users
+    """
+    user_steps.get_users()
