0551f6105ba5dc54ac5146629d4ecd2f10881ab6
Alexander Gromov <agromov@mirantis.com>
2017-03-23 14:27:41 +0300

    Renamed test on cluster deletion using execution
    Change-Id: I5bc97dee46709a74a2ad8e605d5caebd87e3bbcd

--- whale/decapod_ui/tests/test_execution.py
+++ whale/decapod_ui/tests/test_execution.py
@@ -19,12 +19,14 @@ Execution tests
 
 import pytest
 
+from whale import config
+
 
 @pytest.mark.idempotent_id('d055a21c-8cbe-49ec-82eb-360d9973264d')
 def test_create_execution_deploy_cluster(playbook_config,
                                          execution_steps,
                                          ui_configuration_steps):
-    """**Scenario:** Execution may be created in UI.
+    """**Scenario:** Execution for cluster deployment may be created in UI.
 
     **Setup:**
 
@@ -46,3 +48,50 @@ def test_create_execution_deploy_cluster(playbook_config,
     execution = execution_steps.get_last_execution_by_config_id(
         playbook_config['id'])
     execution_steps.check_execution_status(execution['id'])
+
+
+@pytest.mark.idempotent_id('539e6207-de8d-4fd6-aacf-4e1ba95b7892')
+def test_create_execution_purge_cluster(cluster_steps,
+                                        server_steps,
+                                        playbook_config_steps,
+                                        execution_steps,
+                                        ui_configuration_steps):
+    """**Scenario:** Execution for cluster deletion may be created in UI.
+
+    **Steps:**
+
+    #. Get available servers
+    #. Create cluster via API
+    #. Create playbook configuration for cluster deployment via API
+    #. Create execution for cluster deployment via API
+    #. Create playbook configuration for cluster deletion via API
+    #. Create execution for cluster deletion via UI
+    #. Check cluster has been deleted
+
+    **Teardown:**
+
+    #. Delete playbook configuration
+    """
+    server_ids = server_steps.get_server_ids()
+    cluster = cluster_steps.create_cluster()
+
+    playbook_config_deploy = playbook_config_steps.create_playbook_config(
+        cluster_id=cluster['id'],
+        playbook_id=config.PLAYBOOK_DEPLOY_CLUSTER,
+        server_ids=server_ids)
+
+    execution_steps.create_execution(playbook_config_deploy['id'])
+
+    playbook_config_purge = playbook_config_steps.create_playbook_config(
+        cluster_id=cluster['id'],
+        playbook_id=config.PLAYBOOK_PURGE_CLUSTER,
+        server_ids=[])
+
+    ui_configuration_steps.create_execution(
+        playbook_config_purge['data']['name'])
+    execution = execution_steps.get_last_execution_by_config_id(
+        playbook_config_purge['id'])
+    execution_steps.check_execution_status(execution['id'])
+
+    cluster_steps.check_cluster_presence(cluster['id'],
+                                         must_present=False)
